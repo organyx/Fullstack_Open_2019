2860ea5dea446034a2b64fa609a6ce6896963a0d
aleksandr Beliun <aleksandr.beliun@gmail.com>
2019-06-30 18:11:46 +0200

    Part_1 Exercise 1.13

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,18 +1,10 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const App = props => {
-  const [selected, setSelected] = useState(0);
-  const random = Math.floor((Math.random() * anecdotes.length));
-
-  return (
-    <div>
-      {props.anecdotes[selected]}
-      <br/>
-      <button onClick={() => setSelected(random)}>Next One</button>
-    </div>
-  );
-};
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+const Votes = ({ selected, votes }) => <p>Has {votes[selected]} votes</p>;
 
 const anecdotes = [
   'If it hurts, do it more often',
@@ -22,5 +14,28 @@ const anecdotes = [
   'Premature optimization is the root of all evil.',
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ];
+
+const App = props => {
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(Array(6).fill(0));
+
+  const vote = selected => {
+    const copy = [...votes];
+    copy[selected] += 1;
+    setVotes(copy);
+  };
+
+  return (
+    <div>
+      <p>{props.anecdotes[selected]}</p>
+      <Votes votes={votes} selected={selected} />
+      <Button handleClick={() => vote(selected)} text="Vote" />
+      <Button
+        handleClick={() => setSelected(Math.floor(Math.random() * 6))}
+        text="Next One"
+      />
+    </div>
+  );
+};
 
 ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById('root'));
