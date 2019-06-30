d9af67354b398a225c83c3dc2c7fb3dd0d64da36
aleksandr Beliun <aleksandr.beliun@gmail.com>
2019-06-30 15:41:49 +0200

    Part_1 Exercise 1.6

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,63 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const Header = props => {
-  return <h1>{props.course}</h1>;
-};
-
-const Content = props => {
-  const parts = props.parts;
-  console.log('Content', parts);
-  const partsList = parts.map(part => (
-    <Part key={part.name} partNumber={part.name} exercises={part.exercises} />
-  ));
-  return <div>{partsList}</div>;
-};
-
-const Total = props => {
-  const countTotal = function(parts) {
-    var total = 0;
-    parts.forEach(part => {
-      total += part.exercises;
-    });
-    return total;
-  };
-  const total = countTotal(props.total);
-  return <p>Number of Exercises {total}</p>;
-};
-
-const Part = props => {
-  console.log('PART ', props);
-  return (
-    <p>
-      {props.partNumber} {props.exercises}
-    </p>
-  );
-};
-
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const Section = ({ name }) => {
+    return <h1>{name}</h1>;
   };
+
+  const Buttons = props => {
+    return (
+      <div>
+        <button onClick={() => setGood(good + 1)}>Good</button>
+        <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+        <button onClick={() => setBad(bad + 1)}>Bad</button>
+      </div>
+    );
+  };
+
+  const Results = props => {
+    return (
+      <div>
+        <p>Good {good}</p>
+        <p>Neutral {neutral}</p>
+        <p>Bad {bad}</p>
+      </div>
+    );
+  };
+
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total total={course.parts} />
+      <Section name={'Feedback'} />
+      <Buttons />
+      <Section name={'Statistics'} />
+      <Results />
     </div>
   );
 };
