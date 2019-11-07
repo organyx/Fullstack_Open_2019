387027976e4a39a002e0cdaa7d6e39acfe396e5e
Aleksandr Beliun <aleksandr.beliun@gmail.com>
2019-11-07 15:00:06 +0100

    Part_4 Exercise 4.4

--- part4/blog_list/back/utils/list_helper.js
+++ part4/blog_list/back/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  let total = 0
+  blogs.forEach(blog => {
+    total += blog.likes
+  })
+  return total
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
